239ddd9b709dde1d01358e77fe51fc9077032fad
gonzaliaga <gonzaliaga@gmail.com>
2023-11-14 20:09:46 -0300

    cambios vite config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
- 
 
-  export default defineConfig({
-    // base: "/PM4_RESTAURANT_APP/",
-    plugins: [react()],
 
+export default defineConfig({
+  plugins: [react()],
 })
 
