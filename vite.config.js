f4919a9b9000004fce424d844e26eea3b497d623
gonzaliaga <gonzaliaga@gmail.com>
2023-11-14 20:15:25 -0300

    cambios vite config

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust this value based on your needs
+  },
 })
 
