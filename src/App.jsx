ea35ca20ff98955644273ef714245a98191a9b96
gonzaliaga <gonzaliaga@gmail.com>
2023-11-14 19:11:04 -0300

    Pruebas

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import AppRoutes from './routes/navigation/AppRoutes.JSX';
+// import Navigation from './routes/navigation/navigation';
+import Footer from './componentes/footer/Footer';
+
+
+
+function App() {
+  return (
+    <>
+      <AppRoutes/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App;
