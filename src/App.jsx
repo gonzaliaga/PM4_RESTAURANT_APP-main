0841323df00930468ff5a384c803555ba3519950
gonzaliaga <gonzaliaga@gmail.com>
2023-11-14 19:42:32 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import AppRoutes from './routes/navigation/AppRoutes.JSX';
+import AppRoutes from './routes/navigation/AppRoutes';
 // import Navigation from './routes/navigation/navigation';
 import Footer from './componentes/footer/Footer';
 
